efaa70e55a4ffe731d368d8e91774cd23d5a6513
Liubs <liubsyy@gmail.com>
2023-10-21 16:22:16 +0800

    打包

--- src/main/java/com/liubs/findinstances/jvmti/InstancesOfClass.java
+++ src/main/java/com/liubs/findinstances/jvmti/InstancesOfClass.java
@@ -1,5 +1,6 @@
 package com.liubs.findinstances.jvmti;
 
+import java.io.*;
 import java.net.URL;
 
 /**
@@ -8,22 +9,52 @@ import java.net.URL;
  * @date 2023/10/20 10:08 PM
  **/
 public class InstancesOfClass {
+
     static {
-        String bootLib = null;
+        String nativeLib = null;
         String osName = System.getProperty("os.name").toLowerCase();
         if(osName.contains("mac")) {
-            bootLib = "findins.dylib";
+            nativeLib = "findins.dylib";
         }else if(osName.contains("linux")) {
-            bootLib = "findins.so";
+            nativeLib = "findins.so";
         }else if(osName.contains("windows")) {
-            bootLib = "findins.dll";
+            nativeLib = "findins.dll";
         }
-        if(null == bootLib) {
+        if(null == nativeLib) {
             throw new UnsupportedOperationException("不支持当前操作系统");
         }
-        URL bootLibURL = InstancesOfClass.class.getClassLoader().getResource(bootLib);
-        System.load(bootLibURL.getPath());
+        URL nativeLibURL = InstancesOfClass.class.getClassLoader().getResource(nativeLib);
+        if(nativeLibURL.getPath().contains("jar!")) {
+            //读取jar包里面的链接库
+            loadLibraryFromJar(nativeLib);
+        }else {
+            System.load(nativeLibURL.getPath());
+        }
+
     }
+
+
+    private static void loadLibraryFromJar(String nativeLib) {
+        String path = InstancesOfClass.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        
+        File curDir = new File(path);
+        curDir = curDir.getParentFile();
+        
+        File nativeLibFile = new File(curDir.getAbsolutePath()+"/"+nativeLib);
+        nativeLibFile.deleteOnExit();
+
+        try (InputStream is = InstancesOfClass.class.getResourceAsStream("/"+nativeLib); OutputStream os = new FileOutputStream(nativeLibFile)) {
+            byte[] buffer = new byte[1024];
+            int readBytes;
+            while ((readBytes = is.read(buffer)) != -1) {
+                os.write(buffer, 0, readBytes);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.load(nativeLibFile.getAbsolutePath());
+    }
+
 
     /**
      * native方法 : 返回所有的实例对象
