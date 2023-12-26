6abf2c8340aa3423a189e29bf845aa19f6cebae9
Liubsyy <liubsyy@gmail.com>
2023-12-26 12:05:47 +0800

    兼容SpringBoot嵌套jar中使用

--- src/main/java/com/liubs/findinstances/jvmti/InstancesOfClass.java
+++ src/main/java/com/liubs/findinstances/jvmti/InstancesOfClass.java
@@ -2,6 +2,7 @@ package com.liubs.findinstances.jvmti;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
 
 /**
  * 根据类获取所有实例：基于JVMTI实现
@@ -24,9 +25,13 @@ public class InstancesOfClass {
             throw new UnsupportedOperationException("不支持当前操作系统");
         }
         URL nativeLibURL = InstancesOfClass.class.getClassLoader().getResource(nativeLib);
-        if(nativeLibURL.getPath().contains("jar!")) {
-            //读取jar包里面的链接库
-            loadLibraryFromJar(nativeLib);
+        if(null == nativeLibURL) {
+            System.out.println("找不到jar包内的"+nativeLib);
+        }else if("jar".equals(nativeLibURL.getProtocol())) {
+
+            //读取jar包里面的链接库,SpringBoot嵌套jar也支持
+            loadLibraryFromJar(nativeLibURL,nativeLib);
+
         }else {
             System.load(nativeLibURL.getPath());
         }
@@ -34,16 +39,21 @@ public class InstancesOfClass {
     }
 
 
-    private static void loadLibraryFromJar(String nativeLib) {
-        String path = InstancesOfClass.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        
-        File curDir = new File(path);
+    private static void loadLibraryFromJar(URL nativeLibURL,String nativeLib) {
+
+        String path = nativeLibURL.getPath();
+
+        //[jar路径] file:/Users/liubs/.m2/repository/io/github/liubsyy/FindInstancesOfClass/1.0.1/FindInstancesOfClass-1.0.2.jar!/findins.dylib
+        //[SpringBoot嵌套jar] file:/Users/liubs/IdeaProjects/TestSpring/bin/TestSpring.jar!/BOOT-INF/lib/FindInstancesOfClass-1.0.2.jar!/findins.dylib
+        String jarPath = path.substring("file:".length(), path.indexOf("jar!")+"jar".length());
+
+        File curDir = new File(jarPath);
         curDir = curDir.getParentFile();
         
         File nativeLibFile = new File(curDir.getAbsolutePath()+"/"+nativeLib);
         nativeLibFile.deleteOnExit();
 
-        try (InputStream is = InstancesOfClass.class.getResourceAsStream("/"+nativeLib); OutputStream os = new FileOutputStream(nativeLibFile)) {
+        try (InputStream is = nativeLibURL.openStream(); OutputStream os = Files.newOutputStream(nativeLibFile.toPath())) {
             byte[] buffer = new byte[1024];
             int readBytes;
             while ((readBytes = is.read(buffer)) != -1) {
